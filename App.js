7c458268587fa3b328fa24211dafa14acf797d27
teachershriti1 <shritibadola@gmail.com>
2021-07-06 19:57:51 +0530

    QR code

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StylesSheet,Text,View} from 'react-native'
+import {createAppContainer} from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import TransactionScreen from './screens/bookTransactionScreen'
+import SearchScreen from './screens/searchScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator=createBottomTabNavigator({
+  Transaction:{screen:TransactionScreen},
+  Search:{screen:SearchScreen}
+})
+
+const AppContainer=createAppContainer(TabNavigator)
